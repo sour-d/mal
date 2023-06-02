4736de0b6830d9cf4b3f5a4724334200f4c1a0d4
sour-d <36912347+sour-d@users.noreply.github.com>
2023-06-02 11:47:05 +0530

    step 4 for sourd

--- impls/sourd/step4_if_fn_do.js
+++ impls/sourd/step4_if_fn_do.js
@@ -9,6 +9,55 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const handleDef = (ast, env) => {
+  env.set(ast.value[1], EVAL(ast.value[2], env));
+  return env.get(ast.value[1]);
+};
+
+const handleLet = (ast, env) => {
+  const newEnv = new Env(env);
+  const [_, bindingsList, expression] = ast.value;
+  for (let index = 0; index < bindingsList.value.length; index += 2) {
+    const key = bindingsList.value[index];
+    const value = bindingsList.value[index + 1];
+    newEnv.set(key, EVAL(value, newEnv));
+  }
+  return EVAL(expression, newEnv);
+};
+
+const handleIf = (ast, env) => {
+  const [ifKywd, condition, ifPart, elsePart] = ast.value;
+  const evltdCond = EVAL(condition, env);
+  let condResult = true;
+  if ((evltdCond instanceof MalNil) || (evltdCond instanceof MalBool)) {
+    condResult = evltdCond.value;
+  }
+  return condResult ? EVAL(ifPart, env) : EVAL(elsePart, env);
+}
+const handleDo = (ast, env) => {
+  const [doKywd, ...doList] = ast.value;
+  return doList.reduce((_, list) => EVAL(list, env), new MalNil());
+
+}
+const handleFn = (ast, env) => {
+  const [fnKywd, bindings, body] = ast.value;
+  return (...args) => {
+    const newEnv = new Env(env);
+    if (bindings.length === args.length) throw "Unmatched binding";
+    for (let index = 0; index < bindings.value.length; index++) {
+      if (bindings.value[index].value === '&') {
+        newEnv.set(
+          bindings.value[index + 1],
+          new MalVector(EVAL(args.slice(index), newEnv))
+        );
+        break;
+      }
+      newEnv.set(bindings.value[index], EVAL(args[index], newEnv));
+    }
+    return EVAL(body, newEnv);
+  };
+}
+
 const eval_ast = (ast, env) => {
   if (ast === undefined) return new MalNil();
   if (ast instanceof MalSymbol) return env.get(ast);
@@ -36,48 +85,15 @@ const EVAL = (ast, env) => {
 
     switch (ast.value[0].value) {
       case "def!":
-        env.set(ast.value[1], EVAL(ast.value[2], env));
-        return env.get(ast.value[1]);
+        return handleDef(ast, env);
       case "let*":
-        const newEnv = new Env(env);
-        const [_, bindingsList, expression] = ast.value;
-        for (let index = 0; index < bindingsList.value.length; index += 2) {
-          const key = bindingsList.value[index];
-          const value = bindingsList.value[index + 1];
-          newEnv.set(key, EVAL(value, newEnv));
-        }
-        return EVAL(expression, newEnv);
-
+        return handleLet(ast, env);
       case "if":
-        const [ifKywd, condition, ifPart, elsePart] = ast.value;
-        const evltdCond = EVAL(condition, env);
-        let condResult = true;
-        if ((evltdCond instanceof MalNil) || (evltdCond instanceof MalBool)) {
-          condResult = evltdCond.value;
-        }
-        return condResult ? EVAL(ifPart, env) : EVAL(elsePart, env);
-
+        return handleIf(ast, env);
       case "do":
-        const [doKywd, ...doList] = ast.value;
-        return doList.reduce((_, list) => EVAL(list, env), new MalNil());
-
+        return handleDo(ast, env);
       case "fn*":
-        const [fnKywd, bindings, body] = ast.value;
-        return (...args) => {
-          const newEnv = new Env(env);
-          if (bindings.length === args.length) throw "Unmatched binding";
-          for (let index = 0; index < bindings.value.length; index++) {
-            if (bindings.value[index].value === '&') {
-              newEnv.set(
-                bindings.value[index + 1],
-                new MalVector(EVAL(args.slice(index), newEnv))
-              );
-              break;
-            }
-            newEnv.set(bindings.value[index], EVAL(args[index], newEnv));
-          }
-          return EVAL(body, newEnv);
-        };
+        return handleFn(ast, env);
     }
 
     const [fn, ...args] = eval_ast(ast, env).value;
